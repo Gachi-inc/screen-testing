42debbb7e79e41d14586cc160f8c512d3ebc114f
Komodood <36847935+Komodood@users.noreply.github.com>
2021-04-25 01:36:35 +0500

    Update socketio.js

--- src/core/socketio.js
+++ src/core/socketio.js
@@ -1,3 +1,8 @@
-import { io } from "socket.io-client";
-const socket = io("192.168.1.7:6543");
+const io = require("socket.io-client");
+const socket = io("192.168.1.7:6543", {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
 export default socket;
