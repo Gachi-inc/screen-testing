721ed1f3f1307b510678aefc3b77537f99f7a2a9
Komodood <36847935+Komodood@users.noreply.github.com>
2021-04-25 14:22:45 +0500

    feat(ScreenshotPrewiew)

--- src/components/ScreenshotPrewiev/index.js
+++ src/components/ScreenshotPrewiev/index.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import "./index.css"
-class ScreenshotPreview extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            screenshot: null
-        }
-    }
-    componentDidMount(){}
+import socket from '../../core/socketio.js';
+function ScreenshotPreview (){
+    const [screenshotUrl, setScreenshot] = useState(null);
+      socket.on('image', function(data){
+        displayImage(data);
+        });
+        
+        var displayImage = function(data){
+            var arrayBufferView = new Uint8Array( data.image_data );
+            var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+            var urlCreator = window.URL || window.webkitURL;
+            var imageUrl = urlCreator.createObjectURL( blob );
+            var img = document.getElementById( "img" );
+            img.src = imageUrl;
+        };
 
 
-    render() { 
         return (
         <div className = "Screenshot">
             <h1>Превью сайта</h1>
-           {/* <img width="80%" height="80%" src="https://img.flaticon.com/icons/png/512/58/58672.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF"> */}
-        {/* </img> */}
+            <img id="img" width= "100%" height= "90%" src={screenshotUrl}/>
         </div>  
         );
-    }
 }
  
 export default ScreenshotPreview;
